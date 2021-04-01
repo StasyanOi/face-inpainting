d374736346aca98ef6bd676f0465ca497867644b
StasyanOi <iluvstrwrs@mail.ru>
2021-04-01 18:28:52 +0300

    init

--- env.sh
+++ env.sh
@@ -1,4 +1,9 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install git
-git clone https://github.com/StasyanOi?tab=repositories
+sudo apt install python3.7
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+sudo update-alternatives --config python3sudo update-alternatives --config python3
+sudo apt install python3.7-venv
+python3 -m venv tutorial-env
+source tutorial-env/bin/activate
+pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
