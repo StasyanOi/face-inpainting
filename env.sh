8a283b79108a94952450747a92b6a25caebe3c27
StasyanOi <iluvstrwrs@mail.ru>
2021-04-01 21:02:49 +0300

    init

--- env.sh
+++ env.sh
@@ -16,3 +16,10 @@ echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc
 
 sudo apt update
 sudo apt-get install software-properties-common
+
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+chmod +x Anaconda3-2020.07-Linux-x86_64.sh
+conda create --name tf_gpu
+conda activate tf_gpu
+conda install tensorflow-gpu
