d7daccd6b1c4c55ee8db7165dd983ed078dfcbab
StasyanOi <iluvstrwrs@mail.ru>
2021-04-05 23:11:18 +0300

    added new funcs

--- video.py
+++ video.py
@@ -41,8 +41,8 @@ if __name__ == '__main__':
     predictions = model.predict(features)
     predictions = np.round(predictions[:, :, :, 0]) * 255.0
     for i in range(len(predictions)):
-        predictions[i] = cv2.erode(predictions[i], (3, 3))
-        predictions[i] = cv2.dilate(predictions[i], (15, 15))
+        predictions[i] = cv2.erode(predictions[i], (15, 15))
+        predictions[i] = cv2.dilate(predictions[i], (50, 50))
         cv2.imshow('img.jpg', (predictions[i]).astype("uint8"))
         k = cv2.waitKey(30) & 0xff
         if k == 27:
