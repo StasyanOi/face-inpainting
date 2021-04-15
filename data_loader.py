d90a98bcc8a0a123ea616bc5f58d1daad8d10cd6
StasyanOi <iluvstrwrs@mail.ru>
2021-04-15 17:55:05 +0300

    added new funcs

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import os
 
 import scipy
 import dataset
+import tensorflow
 from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ import random
 
 
 class DataLoader():
-    def __init__(self, dataset_name, img_res=(128, 128)):
+    def __init__(self, dataset_name="test", img_res=(128, 128)):
         self.dataset_name = dataset_name
         self.img_res = img_res
 
@@ -23,7 +24,7 @@ class DataLoader():
         labels = "train_data/medical/CelebA-HQ-img-256-256-labels"
         merged = "train_data/medical/CelebA-HQ-img-256-256-merged"
         masked = "train_data/medical/CelebA-HQ-img-256-256"
-        indexes = self.get_randoms(batch=128)
+        indexes = self.get_randoms(batch=64)
         dir_list = dataset.sort_names(os.listdir(merged))
         images = [dir_list[indexes[i]] for i in range(len(indexes))]
 
@@ -36,6 +37,18 @@ class DataLoader():
         masks = masks / 255
 
         return potential_output, input, masks
+
+    def load_img(self, dir, img_name):
+
+        images = []
+        feature = tensorflow.keras.preprocessing.image.load_img(dir + "/" + img_name, color_mode="rgb")
+        input_arr_feature = tensorflow.keras.preprocessing.image.img_to_array(feature)
+        images.append(input_arr_feature)
+
+        batch_feature = np.array(images)  # Convert single image to a batch.
+
+        batch_feature = batch_feature / 127.5 - 1
+        return batch_feature
 
     # def load_batch(self, batch_size=1, is_testing=False):
     #     imgs_A = np.array(imgs_A) / 127.5 - 1.
