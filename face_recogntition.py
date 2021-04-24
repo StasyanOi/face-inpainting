f9a36df06267c44b5c1bdc021b1c6b162a99be8a
StasyanOi <iluvstrwrs@mail.ru>
2021-04-24 15:36:14 +0300

    added new funcs

---
+++ face_recogntition.py
@@ -0,0 +1,28 @@
+import face_recognition
+import os
+import cv2.cv2 as cv2
+
+def sort_names(dir):
+    ints = []
+    for i in range(len(dir)):
+        ints.append(int(dir[i].split(".")[0]))
+    ints.sort()
+    for i in range(len(dir)):
+        dir[i] = str(ints[i]) + ".png"
+    return dir
+
+if __name__ == '__main__':
+
+    known_image = face_recognition.load_image_file("compare/me.png")
+    known_encoding = face_recognition.face_encodings(known_image)[0]
+
+    files = sort_names(os.listdir("compare/generated/"))
+    img = []
+    true = 0
+    for i in range(0, len(files)):
+        unknown_image = face_recognition.load_image_file("compare/generated/" + files[i])
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+        if (results[0] == True):
+            true = true + 1
+        print(true / len(files))
